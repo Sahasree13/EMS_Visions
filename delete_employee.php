586d72d003c0ac90edbaa0d923505a4627eef4e2
Sahasree <sahasreechatterjee@gmail.com>
2021-12-29 11:17:15 +0530

    Initial commit

---
+++ delete_employee.php
@@ -0,0 +1,11 @@
+<?php
+require 'conn.php';
+$id=$_GET['e_id'];
+$sql = "DELETE FROM employees WHERE e_id='$id' ";
+if(mysqli_query($conn,$sql)){
+    //echo "Record deleted successfully";
+    header('Location:dash.php');
+}else{
+    echo "Error updating record:".mysqli_error($conn);
+}
+?>
